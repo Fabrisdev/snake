6cbc2b817be4d87542510b45feca0629610f9405
fabrisdev <ezepuntocom@hotmail.com>
2023-02-11 00:23:51 -0300

    Finally all done

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { sleep } from './utils'
-import Snake from './snake'
+import Snake, { Apple } from './snake'
 import { death_animation } from './animations'
 
 const CANVAS_SIZE = 550
@@ -10,8 +10,13 @@ const BORDERS = {
     down: 500,
 }
 const SNAKE_SIZE = 50
-const NEXT_FRAME_WAIT = 1000
-
+const NEXT_FRAME_WAIT = 200
+const START_LENGTH = 5
+const START_POSITION = Object.freeze({
+    x: 300,
+    y: 250,
+})
+//Putod el que mama vega
 const canvas = document.querySelector('#game') as HTMLCanvasElement
 canvas.width = CANVAS_SIZE
 canvas.height = CANVAS_SIZE
@@ -20,15 +25,11 @@ const ctx = canvas.getContext('2d')
 if(!ctx) throw '¿Tu navegador no soporta canvas? Yo que tú me compraría una pc xd'
 const snake = new Snake({
     size: SNAKE_SIZE,
-    start_position: {
-        x: 200,
-        y: 0,
-    },
-    start_length: 3,
+    start_position: START_POSITION,
+    start_length: START_LENGTH,
     borders: BORDERS,
-    ctx,
 })
-let key_press_allowed = true
+const apple = new Apple(SNAKE_SIZE, snake)
 
 async function draw_loop(){
     if(!ctx) throw '¿Tu navegador no soporta canvas? Yo que tú me compraría una pc xd'
@@ -38,13 +39,17 @@ async function draw_loop(){
 
     draw_snake()
 
+    //APPLE
+    ctx.fillStyle = '#f33'
+    ctx.fillRect(apple.get_position().x, apple.get_position().y, SNAKE_SIZE, SNAKE_SIZE)
+    apple.check_eaten()
+
     //DRAW GRID
     for(let i = 0; i < CANVAS_SIZE; i += SNAKE_SIZE){
         for(let j = 0; j < CANVAS_SIZE; j += SNAKE_SIZE){
             ctx.strokeRect(i, j, SNAKE_SIZE, SNAKE_SIZE)
         }
     }
-    key_press_allowed = true
     await sleep(NEXT_FRAME_WAIT)
     draw_loop()
 }
@@ -57,22 +62,20 @@ snake.on('DEATH', () => {
 function draw_snake(){
     if(!ctx) throw '¿Tu navegador no soporta canvas? Yo que tú me compraría una pc xd'
     if(!snake.get_is_alive()) return
+    snake.update()
     const snake_body = snake.get_body_parts()
     ctx.fillStyle = '#444'
-    snake.update()
     snake_body.map(part => {
         ctx.fillRect(part.x, part.y, SNAKE_SIZE, SNAKE_SIZE)
     })
     ctx.fillStyle = '#555'
-    ctx.fillRect(snake.get_position().x, snake.get_position().y, SNAKE_SIZE, SNAKE_SIZE)
+    ctx.fillRect(snake_body[0].x, snake_body[0].y, SNAKE_SIZE, SNAKE_SIZE)
 }
 
 draw_loop()
 
 document.addEventListener('keydown', event => {
     const { key } = event
-    if(!key_press_allowed) return
-    console.log(key)
     if(key === 'ArrowRight' || key === 'd')
         snake.set_moving_direction('RIGHT')
     if(key === 'ArrowDown' || key === 's')
@@ -81,6 +84,14 @@ document.addEventListener('keydown', event => {
         snake.set_moving_direction('LEFT')
     if(key === 'ArrowUp' || key === 'w')
         snake.set_moving_direction('UP')
+})
 
-    key_press_allowed = false
+const restart_game_button = document.querySelector('#restart-game-btn')
+restart_game_button?.addEventListener('click', () => {
+    snake.regenerate()
+    snake.on('DEATH', () => {
+        console.log('muerto')
+        death_animation(ctx, SNAKE_SIZE)
+    })
+    apple.regenerate()
 })
--- src/snake.ts
+++ src/snake.ts
@@ -1,4 +1,5 @@
 import { sleep } from './utils'
+import _ from 'lodash'
 
 export type Position = {
     x: number,
@@ -13,12 +14,10 @@ type UpdatedPosition = {
 class MovableObject{
     private size
     private position
-    private ctx
 
-    constructor(size: number, position: Position, ctx: CanvasRenderingContext2D){
+    constructor(size: number, position: Position){
         this.size = size
         this.position = position
-        this.ctx = ctx
     }
 
     get_size(){
@@ -43,36 +42,40 @@ class MovableObject{
 
 type Directions = 'UP' | 'DOWN' | 'LEFT' | 'RIGHT' | 'STOPPED'
 
-type Borders = {
+type Borders = Readonly<{
     right: number,
     left: number,
     up: number,
     down: number,
-}
+}>
 
-type SnakeProps = {
+type SnakeProps = Readonly<{
     size: number,
-    start_position: Position,
+    start_position: Readonly<Position>,
     start_length: number,
-    borders: Borders,
-    ctx: CanvasRenderingContext2D,
-}
+    borders: Readonly<Borders>,
+}>
 
 type Events = 'DEATH' | 'WIN'
 
-export default class Snake extends MovableObject{
+export default class Snake{
+    private moved_times_with_current_direction = 1
     private borders
     private moving_direction: Directions = 'STOPPED'
     private is_alive = true
-    private body_parts: Position[] = []
-    private length
+    private body: Position[] = []
+    private size
+    private start_position
+    private start_length
 
-    constructor({ size, start_position, start_length, borders, ctx }: SnakeProps){
-        super(size, start_position, ctx)
+    constructor({ size, start_position, start_length, borders }: SnakeProps){
+        this.size = size
         this.borders = borders
-        this.length = start_length
-        for(let i = 1; i <= this.length; i++){
-            this.body_parts.push({
+        this.start_position = start_position
+        this.start_length = start_length
+        this.body.push(structuredClone(start_position))
+        for(let i = 1; i <= start_length; i++){
+            this.body.push({
                 x: start_position.x - size * i,
                 y: start_position.y,
             })
@@ -80,16 +83,33 @@ export default class Snake extends MovableObject{
     }
 
     get_body_parts(){
-        return this.body_parts
+        return this.body
+    }
+
+    regenerate(){
+        this.is_alive = true
+        this.moving_direction = 'STOPPED'
+        this.moved_times_with_current_direction = 1
+        this.body = []
+        this.body.push(structuredClone(this.start_position))
+        for(let i = 1; i <= this.start_length; i++){
+            this.body.push({
+                x: this.start_position.x - this.size * i,
+                y: this.start_position.y,
+            })
+        }
     }
 
     set_moving_direction(direction: Directions){
         if(!this.is_alive) return
+        if(direction === 'LEFT' && this.get_moving_direction() === 'STOPPED') return
         if(direction === 'RIGHT' && this.get_moving_direction() === 'LEFT') return
         if(direction === 'LEFT' && this.get_moving_direction() === 'RIGHT') return
         if(direction === 'DOWN' && this.get_moving_direction() === 'UP') return
         if(direction === 'UP' && this.get_moving_direction() === 'DOWN') return
+        if(this.moved_times_with_current_direction === 0) return
         this.moving_direction = direction
+        this.moved_times_with_current_direction = 0
     }
 
     get_moving_direction(){
@@ -104,47 +124,80 @@ export default class Snake extends MovableObject{
         return this.is_alive
     }
 
+    get_size(){
+        return this.size
+    }
+
+    update_position(index: number, position: UpdatedPosition){
+        if(position.add_x)
+            this.body[index].x += position.add_x
+        if(position.add_y)
+            this.body[index].y += position.add_y
+    }
+
     private move(){
         if(this.get_moving_direction() === 'UP')
-            this.update_position({
-                add_y: -50,
+            this.update_position(0, {
+                add_y: -this.size,
             })
         if(this.get_moving_direction() === 'DOWN')
-            this.update_position({
-                add_y: 50,
+            this.update_position(0, {
+                add_y: this.size,
             })
         if(this.get_moving_direction() === 'LEFT')
-            this.update_position({
-                add_x: -50,
+            this.update_position(0, {
+                add_x: -this.size,
             })   
         if(this.get_moving_direction() === 'RIGHT')
-            this.update_position({
-                add_x: 50,
+            this.update_position(0, {
+                add_x: this.size,
             })  
+        this.moved_times_with_current_direction += 1
         this.check_collisions()
     }
 
     private move_body_parts(){
         if(this.get_moving_direction() === 'STOPPED') return
-        this.body_parts[1] = this.body_parts[0] 
-        this.body_parts[0] = this.get_position()
-        
+        for(let i = this.body.length - 1; i > 0; i--){
+            this.body[i] = structuredClone(this.body[i - 1])
+        }
+
     }
 
     private check_collisions(){
-        if(this.get_position().x > this.get_borders().right) 
-            this.kill()
-        if(this.get_position().x < this.get_borders().left)
-            this.kill()
-        if(this.get_position().y > this.get_borders().down)
-            this.kill()
-        if(this.get_position().y < this.get_borders().up)
-            this.kill()
+        this.body.map(part => {
+            if(part.x > this.get_borders().right) 
+                this.kill()
+            if(part.x < this.get_borders().left)
+                this.kill()
+            if(part.y > this.get_borders().down)
+                this.kill()
+            if(part.y < this.get_borders().up)
+                this.kill()
+        })
+        for(let i = 0; i < this.body.length; i++){
+            for(let j = 0; j < this.body.length; j++){
+                if(i === j) continue
+                const part_one = this.body[i]
+                const part_two = this.body[j]
+                if(_.isEqual(part_one, part_two)){
+                    this.kill()
+                    return
+                }
+            }
+        }
     }
 
     private kill(){
         this.is_alive = false
         this.set_moving_direction('STOPPED')   
+    }
+
+    /**
+     * Calls this event when the snake ate an apple
+     */
+    ate_apple(){
+        this.body.push(structuredClone(this.body.at(-1)))
     }
 
     async on(event_type: Events, callback: () => void){
@@ -163,17 +216,56 @@ export default class Snake extends MovableObject{
     }
 }
 
-class Apple extends MovableObject{
-    private should_draw = true
+export class Apple extends MovableObject{
+    private snake
 
-    constructor(size: number, position: Position, ctx: CanvasRenderingContext2D){
-        super(size, position, ctx)
+    constructor(size: number, snake: Snake){
+        const position = {
+            x: -10000,
+            y: -10000,
+        }
+        super(size, position)
+        this.snake = snake
+        this.set_apple_position()
+    }
+
+    private set_apple_position(){
+        function get_random_number(min: number, max: number) {
+            return Math.random() * (max - min) + min
+        }
+
+        do{
+            const rows = this.snake.get_borders().right / this.snake.get_size()
+            const new_x = Math.floor(get_random_number(0, rows)) * this.snake.get_size()
+            const columns = this.snake.get_borders().down / this.snake.get_size()
+            const new_y = Math.floor(get_random_number(0, columns)) * this.snake.get_size()
+            this.set_position({
+                x: new_x,
+                y: new_y
+            })
+        }while(this.is_apple_inside_snake())
+    }
+
+    regenerate(){
+        this.set_apple_position()
+    }
+
+    private is_apple_inside_snake(){
+        return this.snake.get_body_parts().some(part => _.isEqual(part, this.get_position()))
+    }
+
+    check_eaten(){
+        if(!this.is_apple_inside_snake()) return false
+        console.log('F por la manzanita')
+        this.regenerate()
+        this.snake.ate_apple()
+        return true
     }
 
     /**
      * Call this when the apple is eaten
      */
-    on_eat(){
-        this.should_draw = false
+    async eat(){
+        
     }
 }
